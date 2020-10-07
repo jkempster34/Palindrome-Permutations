c4df93ce94433787a248296ea800eec86fc2b6c5
Joseph Kempster <jkempster34@gmail.com>
2020-10-07 12:31:21 +0100

    Add empty console application

---
+++ PalindromePermutations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PalindromePermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
