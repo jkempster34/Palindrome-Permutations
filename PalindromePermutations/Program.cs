b229af6b6910ca9a93c73f1320de1e815e7da449
Joseph Kempster <jkempster34@gmail.com>
2020-10-08 22:14:16 +0100

    Update console program so user can enter a string to be tested

--- PalindromePermutations/Program.cs
+++ PalindromePermutations/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PalindromePermutations
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter string to get palindromic permutations: ");
+            string text = Console.ReadLine();
+
+            if (PalindromePermutationsSolver.TryGeneratePalindromicPermutations(text, out List<string> palindromes))
+            {
+                Console.WriteLine("The palindromes produced from your text are: ");
+                foreach (var palindrome in palindromes)
+                    Console.WriteLine(palindrome);
+            }
+            else
+            {
+                Console.WriteLine("No permutations of this text contain palindromes.");
+            }
+
+            Console.ReadKey();
         }
     }
 }
